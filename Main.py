683c3b0f226bcd6033a3a7bf69279ce4da79bf40
supergamer66000 <42475968+supergamer66000@users.noreply.github.com>
2023-10-30 16:07:13 -0500

    Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
+import os
+import PIL.Image as PIL
 import matplotlib.pyplot as plt
-from time import strftime, gmtime
 import numpy as np
 import cv2
 import keyboard
@@ -9,6 +10,9 @@ from time import strftime, gmtime
 def capture_and_process_video():
     # Open the video capture (use the camera index 0 for the default camera)
     cap = cv2.VideoCapture(0)
+
+    # Lets user Know of the Controls
+
 
     # Sets the Style
     plt.style.use('dark_background')
@@ -66,17 +70,19 @@ def capture_and_process_video():
         plt.figtext(0.02, 0.88, f'Min: [{red_min}, {green_min}, {blue_min}]')
 
         if keyboard.is_pressed('q'):
+            cap.release()
+            plt.close()
             break
 
         if keyboard.is_pressed('w'):
             save_data(means_rgb, frame_rgb, frame)
+        if keyboard.is_pressed('c' or 'g'):
+            cap.release()
+            get_pixel_position(frame)
+            break
 
         # Pause briefly to display the frame
         plt.pause(0.0001)
-
-    # Release the video capture object and close the Matplotlib window
-    cap.release()
-    plt.close()
 
 
 def save_data(means_rgb, frame_rgb, frame):
@@ -99,6 +105,47 @@ def save_data(means_rgb, frame_rgb, frame):
 
     print("Saved!")
     plt.figtext(0.02, 0.84, "Saved!")
+
+def get_pixel_position(frame):
+    # Closes Window
+    plt.close()
+
+    # Initializes the image
+    cv2.imwrite('%TEMP%.png', frame)
+    img = PIL.open('%TEMP%.png', mode='r')
+
+    # Get the tuple from the console
+    pixel_xy = input("Enter pixel x, y: ")
+
+    # Convert the tuple to a list
+    pixel_xy = pixel_xy.split(",")
+
+    # Changes Text color
+    red_text = "\033[91m"
+    reset_color = "\033[0m"
+
+    # Trys to get pixel color
+    try:
+        pixel_rgb = img.getpixel(xy=(int(pixel_xy[0]), int(pixel_xy[1])))
+        print(pixel_rgb)
+        img.close() # Closes Var Img
+        os.remove('%TEMP%.png') # Deletes the image
+
+    # Checks for a Value ERROR
+    except ValueError as ERROR:
+        img.close()
+        print(red_text + str(ERROR) + "\nPlease dont use () type x, y" + reset_color)
+        get_pixel_position(frame)
+
+    #Checks for a Index ERROR
+    except IndexError as ERROR:
+        img.close()
+        print(red_text + str(ERROR) + f"\nMax image is size is {img.size}" + reset_color)
+        get_pixel_position(frame)
+
+    # Reruns the Main Program
+    capture_and_process_video()
+
 
 
 if __name__ == "__main__":
